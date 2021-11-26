4f20c2096205826e73a054fe9fbc89547957bf44
Canberk Sönmez <canberk.sonmez@epfl.ch>
2021-11-26 17:16:45 +0100

    Plots.

--- eda/main.py
+++ eda/main.py
@@ -12,11 +12,17 @@ DATA_DIR = os.path.dirname(os.path.abspath(__file__)) + "/output"
 area = AreaDatabase(DATA_DIR)
 
 def assign_handlers():
+    """
+    Enables the estimation of the dot products and fp2bfp converters.
+    """
+
     area.add_on_miss(DotProductAreaHandler(area))
     
     fp2bfp_area_handler = FloatingPointToBlockFloatingPointAreaHandler(area)
     fp2bfp_area_handler.add_estimator(FloatingPoint.bfloat16, FixedPointWithExponent(10, 4), [2, 4, 6, 32])
+    fp2bfp_area_handler.add_estimator(FloatingPoint.bfloat16, FixedPointWithExponent(10, 5), [2, 4, 6, 32])
     fp2bfp_area_handler.add_estimator(FloatingPoint.bfloat16, FixedPointWithExponent(10, 6), [2, 4, 6, 32])
+    fp2bfp_area_handler.add_estimator(FloatingPoint.bfloat16, FixedPointWithExponent(10, 7), [2, 4, 6, 32])
     fp2bfp_area_handler.add_estimator(FloatingPoint.bfloat16, FixedPointWithExponent(10, 8), [2, 4, 6, 32])
 
     area.add_on_miss(fp2bfp_area_handler)
@@ -72,27 +78,34 @@ def main():
 
         plt.savefig("test.png")
 
-    print(area(Add(FloatingPoint.ieee_fp32)))
-    print(area(Multiply(FloatingPoint.ieee_fp32)))
-    print(area(Add(UInt(10))))
-    print(area(Add(SInt(16))))
-    print(area(Multiply(SInt(8))))
+        print(area(Add(FloatingPoint.ieee_fp32)))
+        print(area(Multiply(FloatingPoint.ieee_fp32)))
+        print(area(Add(UInt(10))))
+        print(area(Add(SInt(16))))
+        print(area(Multiply(SInt(8))))
+    
+    def compare_against_fp(gen_fp: FloatingPoint, fp_name):
+        block_sizes = np.linspace(0, 50, 1000)
+        fp_cost = cost_fpvec(gen_fp)(block_sizes)
 
-    block_sizes = np.linspace(0, 100, 1000)
-    y1 = cost_hbfp(FixedPointWithExponent(10, 8), FloatingPoint.bfloat16)(block_sizes)
-    y2 = cost_hbfp(FixedPointWithExponent(10, 6), FloatingPoint.bfloat16)(block_sizes)
-    y3 = cost_hbfp(FixedPointWithExponent(10, 4), FloatingPoint.bfloat16)(block_sizes)
-    y0 = cost_fpvec(FloatingPoint.ieee_fp32)(block_sizes)
+        plt.figure()
 
-    plt.figure()
+        plt.title(f"{fp_name} vs. HBFP$n$ Area Comparison")
+        plt.xlabel("Block Size")
+        plt.ylabel("Area Ratio")
 
-    plt.plot(block_sizes, y0 / y1)
-    plt.plot(block_sizes, y0 / y2)
-    plt.plot(block_sizes, y0 / y3)
+        for n in [8, 7, 6, 5, 4]:
+            hbfp_cost = cost_hbfp(FixedPointWithExponent(10, n), FloatingPoint.bfloat16)(block_sizes)
+            plt.plot(block_sizes, fp_cost / hbfp_cost, label=f"{fp_name}/HBFP{n}")
 
-    plt.grid()
+        plt.grid()
+        plt.legend()
 
-    plt.savefig("test.png")
+        plt.savefig(f"{fp_name}_hbfp_comparison.png".lower())
+        plt.savefig(f"{fp_name}_hbfp_comparison.svg".lower())
+    
+    compare_against_fp(FloatingPoint.ieee_fp32, "FP32")
+    compare_against_fp(FloatingPoint.bfloat16, "bfloat16")
 
 
 if __name__ == "__main__":
